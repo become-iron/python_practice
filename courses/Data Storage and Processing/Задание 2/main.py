e1fbaa920e39027aee8fcb6a26242395a4365338
Burkov Nikita <nikitaburkov@yandex.ru>
2018-09-18 23:15:53 +0300

    Data Storage and Processing: simplify script for task #2

--- courses/Data Storage and Processing/Задание 2/main.py
+++ courses/Data Storage and Processing/Задание 2/main.py
@@ -1,58 +1,39 @@
 import csv
-from collections import namedtuple
-
-initial_data = [
-    {
-        'file_path': './boys.csv',
-        'names': ['Денис', 'Глеб', 'Леон', 'Тихон']
-    },
-    {
-        'file_path': './girls.csv',
-        'names': ['Карина', 'Евгения', 'Яна']
-    }
-]
-
-NameMonthStatistics = namedtuple('NameMonthStatistics', ['number_of_persons', 'year', 'month'])
 
 
 def process_data(file_path, names):
+    names_index = {name: [] for name in names}
+
     with open(file_path) as file:
         data = csv.reader(file, delimiter=';')
         next(data)  # skip header
-
-        names_index = {name: [] for name in names}
 
         for name_month_data in data:
             name = name_month_data[1]
 
             if name in names_index:
-                month_statistics = NameMonthStatistics(
-                    number_of_persons=int(name_month_data[2]),
-                    year=int(name_month_data[4]),
-                    month=name_month_data[5]
-                )
-                names_index[name].append(month_statistics)
+                names_index[name].append(int(name_month_data[2]))  # number_of_persons
 
-        for (name, name_data) in names_index.items():
-            name_data.sort(key=lambda n: n.number_of_persons)
+    for (name, name_data) in names_index.items():
+        name_data.sort()
 
-            # calc quantiles
-            middle_index = int(len(name_data) / 2)
+        # calc quantiles
+        middle_index = int(len(name_data) / 2)
 
-            q1 = get_median(name_data[:middle_index])
-            # q2 = get_median(name_data)
-            q3 = get_median(name_data[middle_index:])
-            iqr = q3 - q1
+        q1 = get_median(name_data[:middle_index])
+        # q2 = get_median(name_data)
+        q3 = get_median(name_data[middle_index:])
+        iqr = q3 - q1
 
-            low_bound = q1 - 1.5 * iqr
-            high_bound = q3 + 1.5 * iqr
+        low_bound = q1 - 1.5 * iqr
+        high_bound = q3 + 1.5 * iqr
 
-            ejection = any(
-                month_data.number_of_persons < low_bound or month_data.number_of_persons > high_bound
-                for month_data in name_data
-            )
+        ejection = any(
+            month_data < low_bound or month_data > high_bound
+            for month_data in name_data
+        )
 
-            print(name, ejection)
+        yield name, ejection
 
 
 def get_median(name_data):
@@ -60,20 +41,28 @@ def get_median(name_data):
     median_index = int(length / 2)
 
     if length % 2 == 0:
-        first = name_data[median_index].number_of_persons
-        second = name_data[median_index + 1].number_of_persons
+        first = name_data[median_index]
+        second = name_data[median_index + 1]
         median = int((first + second) / 2)
     else:
         median_index += 1
-        median = name_data[median_index].number_of_persons
+        median = name_data[median_index]
 
     return median
 
 
-def main():
-    for d in initial_data:
-        process_data(**d)
-
-
 if __name__ == '__main__':
-    main()
+    initial_data = [
+        {
+            'file_path': './boys.csv',
+            'names': ['Денис', 'Глеб', 'Леон', 'Тихон']
+        },
+        {
+            'file_path': './girls.csv',
+            'names': ['Карина', 'Евгения', 'Яна']
+        }
+    ]
+
+    for params in initial_data:
+        for result in process_data(**params):
+            print(*result)
